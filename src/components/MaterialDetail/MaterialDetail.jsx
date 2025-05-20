ba83382acae3e51dfc75eb0b5ef91a1433f17f92
unknown <vgudojt@mail.ru>
2025-05-20 16:34:05 +0300

    add modalWindow

--- src/components/MaterialDetail/MaterialDetail.jsx
+++ src/components/MaterialDetail/MaterialDetail.jsx
@@ -1,9 +1,34 @@
-
+import React, { useState } from 'react';
 import css from "./materialDetail.module.css";
 
 export const MaterialDetail = () => {
     const requireImages = require.context('../../avant/', false, /\.(png|jpe?g|svg)$/);
     const images = requireImages.keys().map((filename) => requireImages(filename));
+
+      // Состояние для модального окна и выбранного изображения
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  // Функция открытия модального окна
+  const openModal = (image) => {
+    setSelectedImage(image);
+    setIsModalOpen(true);
+  };
+
+  // Функция закрытия модального окна
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage(null);
+  };
+
+    // Обработчик клика по модальному окну
+    const handleModalClick = (e) => {
+      // Закрываем модальное окно только если клик был по самому .modal (фону)
+      if (e.target.classList.contains(css.modal)) {
+        closeModal();
+      }
+    };
+
     return    <section className={css.main}>
     <h1 className={css.title}>
       Avant Quartz
@@ -12,8 +37,23 @@ export const MaterialDetail = () => {
 {images.map((image, index) => (
           <img key={index} className={css.img}         
           src={image}
-          alt={`Photo ${index + 1}`} />
+          alt={`Photo ${index + 1}`} 
+          onClick={() => openModal(image)} // Открываем модальное окно при клике
+          />
+          
       ))}
     </div>
+
+    
+      {/* Модальное окно */}
+      {isModalOpen && (
+        <div className={css.modal} onClick={handleModalClick}>
+          <span className={css.modalClose} onClick={closeModal}>
+            ×
+          </span>
+          <img className={css.modalContent} src={selectedImage} alt="Full screen" />
+        </div>
+      )}
+
   </section>
 }
