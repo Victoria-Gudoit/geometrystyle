716414f1c9631069ddc8a127b291030940e119d4
unknown <vgudojt@mail.ru>
2025-05-20 15:46:11 +0300

    refactoring all code

--- src/components/MaterialDetail/MaterialDetail.jsx
+++ src/components/MaterialDetail/MaterialDetail.jsx
@@ -2,18 +2,18 @@
 import css from "./materialDetail.module.css";
 
 export const MaterialDetail = () => {
-    // const requireImages = require.context('../../avant/', false, /\.(png|jpe?g|svg)$/);
-    // const images = requireImages.keys().map((filename) => requireImages(filename));
+    const requireImages = require.context('../../avant/', false, /\.(png|jpe?g|svg)$/);
+    const images = requireImages.keys().map((filename) => requireImages(filename));
     return    <section className={css.main}>
     <h1 className={css.title}>
       Avant Quartz
     </h1>  
-    {/* <div  className={css.container}>
+    <div  className={css.container}>
 {images.map((image, index) => (
           <img key={index} className={css.img}         
           src={image}
           alt={`Photo ${index + 1}`} />
       ))}
-    </div> */}
+    </div>
   </section>
 }
