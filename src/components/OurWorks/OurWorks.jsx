ce2b8efaad6003fc2ab3189d8b059b2f0148bd28
unknown <vgudojt@mail.ru>
2025-05-24 20:01:50 +0300

    add smallModal

--- src/components/OurWorks/OurWorks.jsx
+++ src/components/OurWorks/OurWorks.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./works.module.css";
 import workOne from "../../img/bathroom.webp";
 import workTwo from "../../img/ourtable.webp";
@@ -6,6 +6,8 @@ import workThree from "../../img/sill.webp";
 import workFour from "../../img/reception.webp";
 import workFive from "../../img/ku.webp";
 import workSix from "../../img/ourstairs.webp";
+import { Modal } from "../Modal";
+import { SmallModal } from "../SmallModal";
 
 
 const works = [
@@ -48,18 +50,38 @@ const works = [
 ];
 
 export const OurWorks = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [title, setTitle] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [text, setText] = useState(null);
+
+
+  const openModal = (image, title, text) => {
+    setSelectedImage(image);
+    setTitle(title)
+    setText(text)
+    setIsModalOpen(true);
+  };
   return (
     <section className={css.main}>
       <h1 className={css.title}>Примеры работы</h1>
       <div className={css.work}>
         {works.map((work) => (
-          <article key={work.title} className={css.wrapper}>
+          <article key={work.title} className={css.wrapper}  onClick={() => openModal(work.image, work.title, work.subtitle)}>
             <img className={css.img} src={work.image} alt={work.title} />
             <h2 className={css.text}>{work.title}</h2>
             <p className={css.text}>{work.subtitle}</p>
           </article>
         ))}
       </div>
+      <SmallModal
+        isModalOpen={isModalOpen}
+        selectedImage={selectedImage}
+        setSelectedImage={setSelectedImage}
+        setIsModalOpen={setIsModalOpen}
+        title={title}
+        text={text}
+      />
     </section>
   );
 };
