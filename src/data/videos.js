95483c5dd947076422c63144bc3c0b9abfb5bda9
unknown <vgudojt@mail.ru>
2025-08-07 21:50:21 +0300

    add photo in production.jsx

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,8 +1,12 @@
 import video1 from "../video/IMG_7679.MOV";
 import video2 from "../video/IMG_7691.MOV";
+import video3 from "../video/IMG_8354.MOV";
+
 
 import preview1 from "../img/kitchen/w_01.webp"; 
 import preview2 from "../img/bathroom/w_01.webp";
+import preview3 from "../img/bathroom/w_03.webp";
+
 
 export const videos = [
 
@@ -20,5 +24,11 @@ export const videos = [
     title: "Столешница в ванную комнату из кварцевого агломерата Stratos C7109 Cloud Concrete",
     description: "Этапы производства и установки столешницы.",
   },
-
+  {
+    id: 3,
+    src: video3,
+    poster: preview3,
+    title: "Столешница в ванную комнату из кварцевого агломерата Belenco Perlo White",
+    description: "Монтаж кварцевой столешницы, который включает в себя промазывание силиконом швов с последующим удалением скребком излишков силикона, чтобы шов был гладким и ровным.",
+  },
 ];
